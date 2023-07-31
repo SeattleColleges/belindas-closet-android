6e42047f88570d7f1f19123352b55cd28f24e445
RobelHailu <robel.hailu@seattlecolleges.edu>
2023-07-31 13:40:48 -0700

    Back button added and better/cleaner sizing options

--- app/src/main/java/com/example/belindas_closet/screen/AddProduct.kt
+++ app/src/main/java/com/example/belindas_closet/screen/AddProduct.kt
@@ -1,14 +1,24 @@
 package com.example.belindas_closet.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,14 +28,31 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.belindas_closet.Routes
 import com.example.belindas_closet.data.Product
+import com.example.belindas_closet.data.Sizes
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddProductPage(navController: NavHostController) {
     var productName by remember { mutableStateOf("") }
     var productDescription by remember { mutableStateOf("") }
-    var productSize by remember { mutableStateOf("") }
+    var productSize by remember { mutableStateOf(Sizes.SELECT_SIZE) } /* Default size set */
+
+    /* Back arrow that navigates back to login page */
+    TopAppBar(
+        title = { Text("Login") }, /* todo: change destination where arrow navigates to */
+        navigationIcon = {
+            IconButton(
+                onClick = {
+                    navController.navigate(Routes.Login.route) /* Navigate back to login page */
+                }
+            ) {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back")
+            }
+        }
+    )
 
     Column(
         modifier = Modifier
@@ -41,12 +68,12 @@ fun AddProductPage(navController: NavHostController) {
 
         ProductDescriptionField(
             productDescription = productDescription,
-            onDescriptionChange = { newName -> productDescription = newName }
+            onDescriptionChange = { newDescription -> productDescription = newDescription }
         )
 
         ProductSizeField(
             productSize = productSize,
-            onSizeChange = { newName -> productSize = newName }
+            onSizeChange = { newSize -> productSize = newSize }
         )
 
         /* TODO: finish up product button and validation logic */
@@ -54,14 +81,17 @@ fun AddProductPage(navController: NavHostController) {
             onClick = {
                 if (productName.isNotEmpty()
                     && productDescription.isNotEmpty()
-                    && productSize.isNotEmpty()) {
+                    && productSize != Sizes.SELECT_SIZE) {
                     val newProduct = Product(productName, productDescription, productSize)
                     /* TODO: save new product to db or use a list to hold products (ex: List<Product>) */
                 } else {
                     /* TODO: show error message for empty fields */
                 }
             },
-            modifier = Modifier.padding(16.dp).width(200.dp).align(Alignment.CenterHorizontally)
+            modifier = Modifier
+                .padding(16.dp)
+                .width(200.dp)
+                .align(Alignment.CenterHorizontally)
         )
         {
             Text(text = "Add Product")
@@ -97,14 +127,35 @@ fun ProductDescriptionField(productDescription: String, onDescriptionChange: (St
 }
 
 @Composable
-fun ProductSizeField(productSize: String, onSizeChange: (String) -> Unit) {
-    TextField(
-        value = productSize,
-        onValueChange = onSizeChange,
-        label = { Text(text = "Product Description") },
-        singleLine = true,
+fun ProductSizeField(productSize: Sizes, onSizeChange: (Sizes) -> Unit) {
+    val sizes = Sizes.values() /* using enum values directly */
+    var selectedSize by remember { mutableStateOf(productSize) }
+    var isDropdownMenuExpanded by remember { mutableStateOf(false) }
+
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
-    )
+            .clickable { isDropdownMenuExpanded = !isDropdownMenuExpanded }
+    ) {
+        Text(
+            text = "Size: ${selectedSize.name}",
+            modifier = Modifier.padding(16.dp)
+        )
+        DropdownMenu(
+            expanded = isDropdownMenuExpanded,
+            onDismissRequest = { isDropdownMenuExpanded = false }
+        ) {
+            sizes.forEach { size ->
+                DropdownMenuItem(
+                    text = { Text(size.name) },
+                    onClick = {
+                        selectedSize = size
+                        isDropdownMenuExpanded = false
+                        onSizeChange(size) /* passing selected size using callback*/
+                    }
+                )
+            }
+        }
+
+    }
 }
