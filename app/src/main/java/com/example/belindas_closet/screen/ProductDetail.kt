6743e76e55d7b587073d7435ec0b1d983bd36c1d
Nickolas <nickolasram@gmail.com>
2023-10-13 01:15:58 -0700

    Updated product details page

--- app/src/main/java/com/example/belindas_closet/screen/ProductDetail.kt
+++ app/src/main/java/com/example/belindas_closet/screen/ProductDetail.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -126,6 +127,13 @@ fun ProductDetailCard(product: Product, navController: NavController) {
             )
             Text(text = "Size: ${product.size}")
             Text(text = "Description: ${product.description}")
+            TextButton(
+                onClick = {
+                    navController.navigate(Routes.IndividualProduct.route+"/${product.name}")
+                }
+            ) {
+                Text(text = "More Info")
+            }
         }
     }
 }
