9e87471031375297c878f112284aef9b026d1e7a
RobelHailu <robel.hailu@seattlecolleges.edu>
2023-11-06 20:37:24 -0800

    "commented out product info field. testing out ci pr workflow"

--- app/src/main/java/com/example/belindas_closet/screen/AddProduct.kt
+++ app/src/main/java/com/example/belindas_closet/screen/AddProduct.kt
@@ -73,10 +73,11 @@ fun AddProductPage(navController: NavHostController) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        ProductInfoField(
+        // temporarily commented out, testing ci workflow on pr
+        /*ProductInfoField(
             productName = productName,
             onProductChange = { newName -> productName = newName }
-        )
+        )*/
 
         ProductTypeDropdown(
             selectedProductType = selectedProductType,
