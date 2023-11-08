fe4c08cf34ede32d7dabfbd2150c75f136c85034
E. Brink <80282265+brinkbrink@users.noreply.github.com>
2023-11-08 11:27:40 -0800

    Revert "belindas-closet-android_2_97_navigation-bar (#114)"
    This reverts commit 954a3d80a26104cfad528198750343f9826960d5.

--- app/src/main/java/com/example/belindas_closet/screen/Home.kt
+++ app/src/main/java/com/example/belindas_closet/screen/Home.kt
@@ -1,6 +1,5 @@
 package com.example.belindas_closet.screen
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -8,23 +7,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -46,10 +35,8 @@ import com.example.belindas_closet.data.Datasource
 import com.example.belindas_closet.model.Product
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage(navController: NavController) {
-
     Row(
         modifier = Modifier
             .size(125.dp)
