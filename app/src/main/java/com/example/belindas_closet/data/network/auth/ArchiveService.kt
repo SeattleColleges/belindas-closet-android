88be0701c1b719038f1daf8f63600181a13bb9b9
Hanan Osman <hanan.osman@seattlecolleges.edu>
2023-11-21 22:13:44 -0800

    Connect API endpoint to archive button

---
+++ app/src/main/java/com/example/belindas_closet/data/network/auth/ArchiveService.kt
@@ -0,0 +1,42 @@
+package com.example.belindas_closet.data.network.auth
+
+import com.example.belindas_closet.MainActivity
+import com.example.belindas_closet.data.network.dto.auth_dto.ArchiveRequest
+import com.example.belindas_closet.data.network.dto.auth_dto.ArchiveResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+interface ArchiveService {
+    suspend fun archive(archiveRequest: ArchiveRequest) : ArchiveResponse?
+
+    companion object {
+        fun create() : ArchiveService {
+            return ArchiveServiceImpl(
+                client = HttpClient(Android) {
+                    install(ContentNegotiation) {
+                        json(Json {
+                            prettyPrint = true
+                            isLenient = true
+                            ignoreUnknownKeys = true
+                        })
+                    }
+                    install (Logging) {
+                        level = LogLevel.ALL
+                        logger = Logger.DEFAULT
+                    }
+                },
+                getToken = suspend {
+                    MainActivity.getPref().getString("token", "") ?: ""
+                }
+            )
+        }
+    }
+
+}
