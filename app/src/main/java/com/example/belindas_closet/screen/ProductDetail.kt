e624783cec5e465d5f0ead5099c9605308c6afef
Tin Pham <72054441+tinpham5614@users.noreply.github.com>
2023-11-29 18:22:24 -0800

    belinda-closet-android-product-menu-dropdown-01 (#213)
    * add product menu dropdown
    
    * using drawerState instead of expand
    
    ---------
    
    Co-authored-by: E. Brink <80282265+brinkbrink@users.noreply.github.com>

--- app/src/main/java/com/example/belindas_closet/screen/ProductDetail.kt
+++ app/src/main/java/com/example/belindas_closet/screen/ProductDetail.kt
@@ -17,6 +17,8 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Card
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -41,6 +43,7 @@ import com.example.belindas_closet.R
 import com.example.belindas_closet.Routes
 import com.example.belindas_closet.data.Datasource
 import com.example.belindas_closet.model.Product
+import com.example.belindas_closet.model.ProductType
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -83,6 +86,9 @@ fun ProductDetailPage(navController: NavController) {
                         }
                     ) {
                         Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+                    }
+                    DropDownCategoryList(drawerState, navController) {
+                        drawerState = DrawerValue.Closed
                     }
                 }
             )
@@ -155,6 +161,28 @@ fun ProductDetailList(products: List<Product>, navController: NavController) {
             .filter { it.productType.type == MainActivity.getProductType() }
             .filter { !hidden!!.contains(it.id) }) { product ->
             ProductDetailCard(product = product, navController = navController)
+        }
+    }
+}
+@Composable
+fun DropDownCategoryList(drawerState: DrawerValue, navController: NavController, onDismissRequest: () -> Unit = {}) {
+    DropdownMenu(
+        drawerState == DrawerValue.Open,
+        onDismissRequest = onDismissRequest,
+        modifier = Modifier
+            .padding(8.dp)
+    ) {
+        ProductType.values().forEach { productType ->
+            DropdownMenuItem(
+                text = { Text(productType.type) },
+                onClick = {
+                    onDismissRequest()
+                    // navigate to product type page
+                    MainActivity.setProductType(productType.type)
+                    navController.navigate(
+                        Routes.ProductDetail.route)
+                }
+            )
         }
     }
 }
