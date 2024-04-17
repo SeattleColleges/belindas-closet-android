b9988551900e33e56eff57d68f725534c095dee4
Hanan Osman <hanan.osman@seattlecolleges.edu>
2024-04-16 23:58:52 -0700

    Added Dashboard and Sign Out links to profile dropdown

--- app/src/main/java/com/example/belindas_closet/Routes.kt
+++ app/src/main/java/com/example/belindas_closet/Routes.kt
@@ -15,4 +15,5 @@ sealed class Routes (val route: String) {
     object CreatorView: Routes("Creator_View")
     object EditUserRole: Routes("Edit User Role")
     object DonationInfo: Routes("Donation_Info")
+    object Dashboard: Routes("Dashboard")
 }
--- app/src/main/java/com/example/belindas_closet/screen/Home.kt
+++ app/src/main/java/com/example/belindas_closet/screen/Home.kt
@@ -17,6 +17,8 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,6 +26,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -43,10 +49,12 @@ import com.example.belindas_closet.data.Datasource
 import com.example.belindas_closet.model.Product
 import com.example.belindas_closet.model.ProductType
 
-//TODO Add Product Catefories to Navbar
+//TODO Add Product Categories to Navbar
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage(navController: NavController) {
+    var drawerState by remember { mutableStateOf(DrawerValue.Closed) }
+
     TopAppBar(
         title = { Text("") },
         actions = {
@@ -72,13 +80,16 @@ fun HomePage(navController: NavController) {
             }
             IconButton(
                 onClick = {
-                    // add links
+                    drawerState = DrawerValue.Open
                 }
             ) {
                 Icon(
                     imageVector = Icons.Default.AccountCircle,
                     contentDescription = "Profile dropdown"
                 )
+            }
+            ProfileDropdown(drawerState, navController) {
+                drawerState = DrawerValue.Closed
             }
             IconButton(
                 onClick = {
@@ -195,7 +206,7 @@ fun TypeCard(productType: ProductType, navController: NavController) {
 
 @Composable
 fun ProductTypeList(products: List<Product>, navController: NavController) {
-    val typeList = ProductType.values().sortedWith(compareBy { it.type });
+    val typeList = ProductType.values().sortedWith(compareBy { it.type })
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
@@ -232,4 +243,26 @@ fun NSCLogo() {
         painter = painterResource(id = R.drawable.nsc_v_logo),
         contentDescription = stringResource(id = R.string.home_nsc_logo_description)
     )
+}
+
+@Composable
+fun ProfileDropdown(drawerState: DrawerValue, navController: NavController, onDismissRequest: () -> Unit = {}) {
+    DropdownMenu(
+        drawerState == DrawerValue.Open,
+        onDismissRequest = onDismissRequest,
+        modifier = Modifier
+            .padding(8.dp)
+    ) {
+        // Dropdown menu items
+        TextButton(onClick = { navController.navigate(Routes.Dashboard.route) }) {
+            Text("Dashboard", color = Color.Black)
+        }
+        TextButton(onClick = {
+            MainActivity.getPref().edit().remove("token").commit()
+            MainActivity.getPref().edit().remove("userRole").commit()
+            navController.navigate(Routes.Home.route)
+        }) {
+            Text("Sign Out", color = Color.Black)
+        }
+    }
 }
